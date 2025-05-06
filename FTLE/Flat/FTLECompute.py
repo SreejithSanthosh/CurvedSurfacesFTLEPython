9fcd7c46b3d143f72656e77d8200e08f59bd448f
bfencil <133804481+bfencil@users.noreply.github.com>
2025-05-05 18:50:05 -0700

    Update FTLECompute.py

--- FTLE/Flat/FTLECompute.py
+++ FTLE/Flat/FTLECompute.py
@@ -31,17 +31,17 @@ def FTLE_2d_compute(x_initial, y_initial, x_final, y_final, initial_time, final_
                 continue
 
             # Local grid spacing
-            dx = x_initial[i + index_shift, j] - x_initial[i - index_shift, j]
-            dy = y_initial[i, j + index_shift] - y_initial[i, j - index_shift]
+            dx = x_initial[i + 1, j] - x_initial[i - 1, j]
+            dy = y_initial[i, j + 1] - y_initial[i, j - 1]
 
             if dx == 0 or dy == 0:
                 continue
 
             # Compute finite difference deformation gradient ∂Xf/∂X0
-            F[0, 0] = (x_final[i + index_shift, j] - x_final[i - index_shift, j]) / (2 * dx)
-            F[0, 1] = (x_final[i, j + index_shift] - x_final[i, j - index_shift]) / (2 * dy)
-            F[1, 0] = (y_final[i + index_shift, j] - y_final[i - index_shift, j]) / (2 * dx)
-            F[1, 1] = (y_final[i, j + index_shift] - y_final[i, j - index_shift]) / (2 * dy)
+            F[0, 0] = (x_final[i + 1, j] - x_final[i - 1, j]) / (2 * dx)
+            F[0, 1] = (x_final[i, j + 1] - x_final[i, j - 1]) / (2 * dy)
+            F[1, 0] = (y_final[i + 1, j] - y_final[i - 1, j]) / (2 * dx)
+            F[1, 1] = (y_final[i, j + 1] - y_final[i, j - 1]) / (2 * dy)
 
             # Cauchy-Green strain tensor: C = Fᵀ F
             C = F.T @ F
@@ -76,25 +76,25 @@ def FTLE_3d_compute(x_initial, y_initial, z_initial, x_final, y_final, z_final, 
                 if math.isnan(x_initial[x_index, y_index, z_index]) or math.isnan(y_initial[x_index, y_index, z_index]):
                     continue
 
-                dx = x_initial[x_index + index_shift, y_index, z_index] - x_initial[x_index - index_shift, y_index, z_index]
-                dy = y_initial[x_index, y_index + index_shift, z_index] - y_initial[x_index, y_index - index_shift, z_index]
-                dz = z_initial[x_index, y_index, z_index + index_shift] - z_initial[x_index, y_index, z_index - index_shift]
+                dx = x_initial[x_index + 1, y_index, z_index] - x_initial[x_index - 1, y_index, z_index]
+                dy = y_initial[x_index, y_index + 1, z_index] - y_initial[x_index, y_index - 1, z_index]
+                dz = z_initial[x_index, y_index, z_index + 1] - z_initial[x_index, y_index, z_index - 1]
 
                 if dx == 0 or dy == 0 or dz == 0:
                     continue
 
                 # ∂Xf/∂X0 (deformation gradient matrix, F_right)
-                F_right[0, 0] = (x_final[x_index + index_shift, y_index, z_index] - x_final[x_index - index_shift, y_index, z_index]) / (2 * dx)
-                F_right[0, 1] = (x_final[x_index, y_index + index_shift, z_index] - x_final[x_index, y_index - index_shift, z_index]) / (2 * dy)
-                F_right[0, 2] = (x_final[x_index, y_index, z_index + index_shift] - x_final[x_index, y_index, z_index - index_shift]) / (2 * dz)
+                F_right[0, 0] = (x_final[x_index + 1, y_index, z_index] - x_final[x_index - 1, y_index, z_index]) / (2 * dx)
+                F_right[0, 1] = (x_final[x_index, y_index + 1, z_index] - x_final[x_index, y_index - 1, z_index]) / (2 * dy)
+                F_right[0, 2] = (x_final[x_index, y_index, z_index + 1] - x_final[x_index, y_index, z_index - 1]) / (2 * dz)
 
-                F_right[1, 0] = (y_final[x_index + index_shift, y_index, z_index] - y_final[x_index - index_shift, y_index, z_index]) / (2 * dx)
-                F_right[1, 1] = (y_final[x_index, y_index + index_shift, z_index] - y_final[x_index, y_index - index_shift, z_index]) / (2 * dy)
-                F_right[1, 2] = (y_final[x_index, y_index, z_index + index_shift] - y_final[x_index, y_index, z_index - index_shift]) / (2 * dz)
+                F_right[1, 0] = (y_final[x_index + index_shift, y_index, z_index] - y_final[x_index - 1, y_index, z_index]) / (2 * dx)
+                F_right[1, 1] = (y_final[x_index, y_index + index_shift, z_index] - y_final[x_index, y_index - 1, z_index]) / (2 * dy)
+                F_right[1, 2] = (y_final[x_index, y_index, z_index + index_shift] - y_final[x_index, y_index, z_index - 1]) / (2 * dz)
 
-                F_right[2, 0] = (z_final[x_index + index_shift, y_index, z_index] - z_final[x_index - index_shift, y_index, z_index]) / (2 * dx)
-                F_right[2, 1] = (z_final[x_index, y_index + index_shift, z_index] - z_final[x_index, y_index - index_shift, z_index]) / (2 * dy)
-                F_right[2, 2] = (z_final[x_index, y_index, z_index + index_shift] - z_final[x_index, y_index, z_index - index_shift]) / (2 * dz)
+                F_right[2, 0] = (z_final[x_index + 1, y_index, z_index] - z_final[x_index - 1, y_index, z_index]) / (2 * dx)
+                F_right[2, 1] = (z_final[x_index, y_index + 1, z_index] - z_final[x_index, y_index - 1, z_index]) / (2 * dy)
+                F_right[2, 2] = (z_final[x_index, y_index, z_index + 1] - z_final[x_index, y_index, z_index - 1]) / (2 * dz)
 
                 # Cauchy-Green strain tensor
                 C = F_right.T @ F_right
